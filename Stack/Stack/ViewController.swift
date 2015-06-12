320ebed0e8db3327b18613d923ddcac372198f10
hansemannn <hans.knoechel@hs-osnabrueck.de>
2015-06-12 23:03:25 +0200

    Initialize Xcode project

---
+++ Stack/Stack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stack
+//
+//  Created by Hans Knöchel on 12.06.15.
+//  Copyright (c) 2015 Hans Knoechel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
