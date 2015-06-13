1ec6a9d0eac94f7c86a5609cca491c9631e76676
hansemannn <hans.knoechel@hs-osnabrueck.de>
2015-06-13 12:04:26 +0200

    Implement start screen interface, add custom font

---
+++ Stack/Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// Snapkit
+#define COCOAPODS_POD_AVAILABLE_Snapkit
+#define COCOAPODS_VERSION_MAJOR_Snapkit 0
+#define COCOAPODS_VERSION_MINOR_Snapkit 11
+#define COCOAPODS_VERSION_PATCH_Snapkit 0
+
---
+++ Stack/Stack/HomeViewController.swift
@@ -0,0 +1,74 @@
+//
+//  HomeViewController.swift
+//  Stack
+//
+//  Created by Hans Knöchel on 12.06.15.
+//  Copyright (c) 2015 Hans Knoechel. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+@IBDesignable
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var newStackButton: UIButton!
+    
+    @IBAction func addStack() {
+    
+    }
+    
+    @IBInspectable lazy var emptyContainerView = UIView()
+    @IBInspectable lazy var emptyImageView = UIImageView()
+    @IBInspectable lazy var emptyLabel = UILabel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.addUI();
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func addConstraints() {
+        self.emptyContainerView.snp_makeConstraints { (make) -> Void in
+            
+            make.width.equalTo(225)
+            make.height.equalTo(255)
+            make.center.equalTo(self.view)
+        }
+        
+        self.emptyImageView.snp_makeConstraints { (make) -> Void in
+            make.width.equalTo(221)
+            make.height.equalTo(190)
+            make.top.equalTo(0)
+            make.centerX.equalTo(self.emptyContainerView)
+        }
+        
+        self.emptyLabel.snp_makeConstraints { (make) -> Void in
+            make.bottom.equalTo(0)
+            make.centerX.equalTo(self.emptyContainerView)
+        }
+    }
+
+    func addUI() {
+        self.newStackButton.layer.cornerRadius = 5.0;
+                
+        self.emptyImageView.image = UIImage(named: "EmptyStack")
+        
+        self.emptyLabel.text = "Noch kein Stapel\nvorhanden."
+        self.emptyLabel.numberOfLines = 2
+        self.emptyLabel.textAlignment = .Center
+        self.emptyLabel.textColor = UIColor(rgba: "#d1d1d1")
+        self.emptyLabel.font = UIFont(name: "CartoGothicStd-Bold", size: 22.0)
+
+        self.emptyContainerView.addSubview(self.emptyImageView)
+        self.emptyContainerView.addSubview(self.emptyLabel)
+        
+        self.view.addSubview(self.emptyContainerView)
+        
+        self.addConstraints()
+    }
+}
+
